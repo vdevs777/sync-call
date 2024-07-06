dad9f25c63522111cc6071e1071401891576cda5
vdevs777 <joaovsousamnm@gmail.com>
2024-07-06 09:35:48 -0300

    :tada: initial commit!

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,10 @@
+import { Heading, Text } from "@ignite-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Heading>Hello World</Heading>
+      <Text>Test</Text>
+    </>
+  );
+}
